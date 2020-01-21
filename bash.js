bf4b69b0222579e6bce9bef8744401e74cb2e5a9
Manny Garcia <mannyalgarcia@gmail.com>
2020-01-21 12:37:05 -0500

    printed working directory

--- bash.js
+++ bash.js
@@ -2,9 +2,13 @@
 process.stdout.write('prompt > ');
 
 // the std 'data' event fires after a user types a line
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim(); // remove newLine and other whitespace
 
-  process.stdout.write('You typed: ' + cmd);
+  if (cmd === 'pwd') {
+    process.stdout.write('You printed: ' + __dirname);
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
   process.stdout.write('\nprompt > ');
 });
